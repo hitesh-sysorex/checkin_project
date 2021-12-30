f520a71efbdd963c6c11dadf6621a1530e1dbccf
shivanimurthy <hiteshsysorex@gmail.com>
2021-12-30 15:31:44 +0530

    test shared preferences

--- lib/user_setting.dart
+++ lib/user_setting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserSetting extends StatefulWidget {
   const UserSetting({Key? key}) : super(key: key);
@@ -8,8 +9,42 @@ class UserSetting extends StatefulWidget {
 }
 
 class _UserSettingState extends State<UserSetting> {
+  var lat = 5;
+  int? Value;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Text("Shared Preferences : ${Value}"),
+          ElevatedButton(
+              onPressed: () {
+                store(lat);
+              },
+              child: Text("Set Data")),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  getdata();
+                });
+              },
+              child: Text("Get Data")),
+        ],
+      ),
+    );
+  }
+
+  void store(int lat) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('key', lat);
+  }
+
+  getdata() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //Return String
+    Value = prefs.getInt('key');
+    print(Value);
+    return Value;
   }
 }
